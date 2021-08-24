092e2ae5a3256306dccfd2f97e84ea80fc2e5a22
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-24 17:40:53 -0500

    feat: Added profile, patient and doctor  models

--- accounts/models.py
+++ accounts/models.py
@@ -21,3 +21,48 @@ class User(AbstractBaseUser, PermissionsMixin):
         db_table = 'users'
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE, serialize=False)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60, null=True)
+    birth_date = models.DateField()
+    register_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    class Meta:
+        db_table = 'profiles'
+
+
+class Doctor(models.Model):
+    doctors = models.OneToOneField(Profile, auto_created=True, on_delete=models.CASCADE,
+                                   primary_key=True, serialize=False)
+    license_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.license_number
+
+    class Meta:
+        db_table = 'doctors'
+
+
+class Patients(models.Model):
+    patients = models.OneToOneField(Profile, auto_created=True, on_delete=models.CASCADE,
+                                    primary_key=True, serialize=False)
+    sex = models.BooleanField()
+    phone = models.CharField(max_length=12)
+    doi = models.CharField(max_length=15)
+    height = models.FloatField(null=True)
+    weight = models.FloatField(null=True)
+    arm = models.FloatField(null=True)
+    abdominal = models.FloatField(null=True)
+    hip = models.FloatField(null=True)
+    imc = models.FloatField(null=True)
+    tmb = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.doi
+
+    class Meta:
+        db_table = 'patients'
