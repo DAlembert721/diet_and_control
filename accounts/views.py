e819fee263be8e0fc8edd0c310685f0cd4aeb440
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-24 18:23:00 -0500

    feat: Added get and put user by id

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
+from django.http import Http404
 from django.shortcuts import render
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from accounts.serializers import UserSerializer
+from accounts.models import User
+from accounts.serializers import UserSerializer, ProfileSerializer
 
 users_response = openapi.Response('Users description', UserSerializer(many=True))
 user_response = openapi.Response('User description', UserSerializer)
@@ -21,3 +24,24 @@ def register(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@swagger_auto_schema(method='get', responses={200: user_response})
+@swagger_auto_schema(methods=['put'], request_body=UserSerializer)
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+def user_detail(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        raise Http404
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
