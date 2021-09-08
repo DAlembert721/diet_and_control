0e2babdf32e53d72104b6b4f2f2848ff10134e9e
DAlembert721 <dalembert.dmu@gmail.com>
2021-09-07 23:13:25 -0500

    feat: Added harmful habits endpoints

--- habits/urls.py
+++ habits/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from habits.views import physical_activities_list, patient_physical_activities_assignment, \
-    list_physical_activities_by_patient
+    list_physical_activities_by_patient, harmful_habits_list, patient_harmful_habits_assigment, \
+    list_harmful_habits_by_patient
 
 urlpatterns = [
     path('physical_activities/', physical_activities_list, name='physical_activities_list'),
@@ -9,4 +10,9 @@ urlpatterns = [
          patient_physical_activities_assignment, name='patient_physical_activities_assignment'),
     path('patients/<int:patient_id>/physical_activities/', list_physical_activities_by_patient,
          name='list_physical_activities_by_patient'),
+    path('harmful_habits/', harmful_habits_list, name='harmful_habits__list'),
+    path('patients/<int:patient_id>/harmful_habits/<int:harmful_habit_id>/',
+         patient_harmful_habits_assigment, name='patient_harmful_habits_assigment'),
+    path('patients/<int:patient_id>/harmful_habits/', list_harmful_habits_by_patient,
+         name='list_harmful_habits_by_patient'),
 ]
--- habits/views.py
+++ habits/views.py
@@ -8,12 +8,15 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from accounts.models import Patient
-from habits.models import PhysicalActivity
-from habits.serializers import PhysicalActivitySerializer
+from habits.models import PhysicalActivity, HarmfulHabit
+from habits.serializers import PhysicalActivitySerializer, HarmfulHabitSerializer
 
 physical_activities_response = openapi.Response('Physical Activities description',
                                                 PhysicalActivitySerializer(many=True))
 physical_activity_response = openapi.Response('Physical Activity description', PhysicalActivitySerializer)
+harmful_habits_response = openapi.Response('Harmful Habits description',
+                                           HarmfulHabitSerializer(many=True))
+harmful_habit_response = openapi.Response('Harmful Habit description', HarmfulHabitSerializer)
 
 
 @swagger_auto_schema(methods=['post'], request_body=PhysicalActivitySerializer,
@@ -71,4 +74,61 @@ def list_physical_activities_by_patient(request, patient_id):
     if request.method == 'GET':
         physical_activities = PhysicalActivity.objects.filter(patients__user=patient.user)
         serializer = PhysicalActivitySerializer(physical_activities, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@swagger_auto_schema(methods=['post'], request_body=HarmfulHabitSerializer,
+                     responses={201: harmful_habit_response})
+@swagger_auto_schema(method='get', responses={200: harmful_habits_response})
+@permission_classes([IsAuthenticated])
+@api_view(['POST', 'GET'])
+def harmful_habits_list(request):
+    if request.method == 'POST':
+        serializer = HarmfulHabitSerializer(request.data)
+        if serializer.is_valid():
+            serializer.save();
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        harmful_habits = HarmfulHabit.objects.all()
+        serializer = HarmfulHabitSerializer(harmful_habits, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@swagger_auto_schema(method='delete')
+@swagger_auto_schema(method='post', responses={200: harmful_habits_response})
+@permission_classes([IsAuthenticated])
+@api_view(['POST', 'DELETE'])
+def patient_harmful_habits_assigment(request, patient_id, harmful_habit_id):
+    try:
+        patient = Patient.objects.get(user=patient_id)
+    except Patient.DoesNotExist:
+        raise Http404
+    try:
+        harmful_habit = HarmfulHabit.objects.get(id=harmful_habit_id)
+    except HarmfulHabit.DoesNotExist:
+        raise Http404
+    if request.method == 'POST':
+        patient.harmful_habits.add(harmful_habit)
+        patient.save()
+        harmful_habits = HarmfulHabit.objects.filter(patients__user=patient.user)
+        serializer = HarmfulHabitSerializer(harmful_habits, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        patient.harmful_habits.remove(harmful_habit)
+        patient.save()
+        return Response(status=status.HTTP_200_OK)
+
+
+@swagger_auto_schema(method='get', responses={200: harmful_habits_response})
+@permission_classes([IsAuthenticated])
+@api_view(['GET'])
+def list_harmful_habits_by_patient(request, patient_id):
+    try:
+        patient = Patient.objects.get(user=patient_id)
+    except Patient.DoesNotExist:
+        raise Http404
+    if request.method == 'GET':
+        harmful_habits = HarmfulHabit.objects.filter(patients__user=patient.user)
+        serializer = HarmfulHabitSerializer(harmful_habits, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
