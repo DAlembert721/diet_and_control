e235cfd8ffdbf28d85625c6fbb66b0e45c3adeaa
DAlembert721 <dalembert.dmu@gmail.com>
2021-10-06 14:38:22 -0500

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', register, name='register'),
     path('users/<int:user_id>/', user_detail, name='user_detail'),
-    path('profiles/<int:user_id>/', create_profiles, name='create_profiles'),
     path('users/<int:user_id>/profiles/', create_profiles, name='create_profiles'),
     path('profiles/<int:profile_id>/', profile_detail, name='profile_detail'),
     path('patients/<int:patient_id>/', patient_detail, name='patient_detail'),
